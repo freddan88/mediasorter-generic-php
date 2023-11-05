19c2d89235be043817d6f23b5d4ade479dc93771
freddan88 <fredrik@leemann.se>
2023-11-05 12:12:45 +0100

    Increased hex-number

--- album_organizer/app/inc/FileProcessor.php
+++ album_organizer/app/inc/FileProcessor.php
@@ -57,8 +57,8 @@ class FileProcessor
             $helper->createDirectoryIfMissing($directoryName);
             $paddedSeqNumber = str_pad(strval($seqNumber), 8, '0', STR_PAD_LEFT);
             $helper->updateSequenceNumber($seqNumber);
+            $randomHex = bin2hex(random_bytes(2));
 
-            $randomHex = bin2hex(random_bytes(1));
             $newFileName = $this->dateTimeData . '_' . $paddedSeqNumber . '_' . $randomHex . '.' . $this->fileExtension;
             // $newFileName = $this->dateTimeData . '_' . $paddedSeqNumber . '.' . $this->fileExtension;
 
